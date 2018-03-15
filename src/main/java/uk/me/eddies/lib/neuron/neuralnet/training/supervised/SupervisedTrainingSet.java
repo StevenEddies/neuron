209d0d20956210d5c0abdc8c160a88e990e2651c
Steven Eddies <steven@eddies.me.uk>
2018-03-15 21:23:50 +0000

    Added convenience methods to SupervisedTrainingSet.

--- src/main/java/uk/me/eddies/lib/neuron/neuralnet/training/supervised/SupervisedTrainingSet.java
+++ src/main/java/uk/me/eddies/lib/neuron/neuralnet/training/supervised/SupervisedTrainingSet.java
@@ -3,6 +3,7 @@
 package uk.me.eddies.lib.neuron.neuralnet.training.supervised;
 
 import static java.util.Collections.unmodifiableList;
+import static java.util.stream.Collectors.toList;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -21,5 +22,17 @@ public class SupervisedTrainingSet {
 
 	public List<SupervisedTrainingRecord> getRecords() {
 		return records;
+	}
+
+	public List<List<Double>> getInputs() {
+		return getRecords().stream()
+				.map(SupervisedTrainingRecord::getInputs)
+				.collect(toList());
+	}
+
+	public List<List<Double>> getExpectedOutputs() {
+		return getRecords().stream()
+				.map(SupervisedTrainingRecord::getExpectedOutputs)
+				.collect(toList());
 	}
 }
--- src/test/java/uk/me/eddies/lib/neuron/neuralnet/training/supervised/SupervisedTrainingSetTest.java
+++ src/test/java/uk/me/eddies/lib/neuron/neuralnet/training/supervised/SupervisedTrainingSetTest.java
@@ -3,7 +3,9 @@
 package uk.me.eddies.lib.neuron.neuralnet.training.supervised;
 
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -51,5 +53,21 @@ public class SupervisedTrainingSetTest {
 	@Test(expected = NullPointerException.class)
 	public void shouldFailToConstructWithNullRecordValue() {
 		new SupervisedTrainingSet(Arrays.asList(record1, null));
+	}
+
+	@Test
+	public void shouldCollectInputs() {
+		when(record1.getInputs()).thenReturn(Arrays.asList(5D, 6D));
+		when(record2.getInputs()).thenReturn(Arrays.asList(7D, 8D));
+		assertThat(systemUnderTest.getInputs(),
+				contains(Arrays.asList(equalTo(record1.getInputs()), equalTo(record2.getInputs()))));
+	}
+
+	@Test
+	public void shouldCollectExpectedOutputs() {
+		when(record1.getExpectedOutputs()).thenReturn(Arrays.asList(2D, 4D));
+		when(record2.getExpectedOutputs()).thenReturn(Arrays.asList(3D, 5D));
+		assertThat(systemUnderTest.getExpectedOutputs(),
+				contains(Arrays.asList(equalTo(record1.getExpectedOutputs()), equalTo(record2.getExpectedOutputs()))));
 	}
 }
